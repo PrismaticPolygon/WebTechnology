3a2c0a984f4cbbdd2e4f5659ba12bb46378820c8
PrismaticPolygon <domwhite97@gmail.com>
2019-12-06 16:10:20 +0000

    Added whitespace, deleted API

--- app/auth/email.py
+++ app/auth/email.py
@@ -4,11 +4,13 @@ from app.email import send_email
 
 
 def send_password_reset_email(user):
+
     token = user.get_reset_password_token()
-    send_email(_('[Microblog] Reset Your Password'),
+    reset_password_string = _('[Bookler] Reset Your Password')
+
+    send_email(reset_password_string,
                sender=current_app.config['ADMINS'][0],
                recipients=[user.email],
-               text_body=render_template('email/reset_password.txt',
-                                         user=user, token=token),
-               html_body=render_template('email/reset_password.html',
-                                         user=user, token=token))
+               text_body=render_template('email/reset_password.txt',                                       user=user, token=token),
+               html_body=render_template('email/reset_password.html', user=user, token=token)
+    )
